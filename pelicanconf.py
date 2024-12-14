8b424824676b58d6202cf36c38e1da8a9b144218
astock1229 <anthony.stock@microchip.com>
2024-12-14 10:12:27 -0700

    Added Flex theme and publishing configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,8 +18,41 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),)
 
-# Social widget
-SOCIAL = (('GitHub', '#'),)
+# Theme Settings
+THEME = 'themes/Flex'
+SITETITLE = 'StockWorks'
+SITESUBTITLE = 'Exploring Code, ML, and AI'
+SITEDESCRIPTION = 'A technical blog about software development, machine learning, and artificial intelligence'
+SITELOGO = None  # Add a path to your logo if you have one
+FAVICON = None   # Add a path to your favicon if you have one
+BROWSER_COLOR = '#333'
+PYGMENTS_STYLE = 'monokai'
+
+# Flex Theme Settings
+MAIN_MENU = True
+MENUITEMS = (
+    ('Archives', '/archives.html'),
+    ('Categories', '/categories.html'),
+    ('Tags', '/tags.html'),
+)
+
+# Social Links
+SOCIAL = (
+    ('github', 'https://github.com/astock1229'),
+)
+
+# Other Settings
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+
+# Add custom CSS
+CUSTOM_CSS = 'theme/css/custom.css'
+STATIC_PATHS = ['images', 'extra/custom.css']
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'theme/css/custom.css'}
+}
 
 DEFAULT_PAGINATION = 10
 
